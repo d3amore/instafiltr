c2ea8c82f79fdea8d2b218a2ae124ca1abf736a8
Illia Hura <hur42466@spseol.cz>
2024-03-20 13:23:01 +0100

    adoptation to vscode

--- insta.py
+++ insta.py
@@ -1,16 +1,21 @@
 from PIL import Image
+print('Hello!')
+print('Which filter do you want to use?')
+choice =  int(input("1.green/ 2.inverse 3.pink 4.red 5. blue 6.cyberpunk"))
 obrazek = Image.open("dog.jpg")
 sirka, vyska = obrazek.size
 x = 0
-while x < sirka:
-    y = 0
-    while y < vyska:
-        r, g, b = obrazek.getpixel((x,y))
-        prumer = int((r+g+b)/3)
-        obrazek.putpixel((x,y),(min(int(r * 1.5), 255), min(int(g * 0.5), 255), min(int(b * 2.0), 255)))
-        y += 1
-    x += 1
-display(obrazek)
+if choice == "1":
+    while x < sirka:
+        y = 0
+        while y < vyska:
+            r, g, b = obrazek.getpixel((x,y))
+            prumer = int((r+g+b)/3)
+            obrazek.putpixel((x,y),(g, r, b))
+            y += 1
+        x += 1
+    obrazek.save("modified_dog.jpg")    
+    obrazek.show()
 
 #r,r,r
 #r , b, r
