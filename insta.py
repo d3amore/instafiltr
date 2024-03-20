35932ccb7179279dcf673e3120d43786456e9cb7
Illia Hura <hur42466@spseol.cz>
2024-03-20 14:03:32 +0100

    ready with optimisation

--- insta.py
+++ insta.py
@@ -1,64 +1,47 @@
 from PIL import Image
 
-def green_filter():
+def apply_filter(filter_func):
     for x in range(sirka):
         for y in range(vyska):
             r, g, b = obrazek.getpixel((x, y))
-            obrazek.putpixel((x, y), (g, r, b))
+            obrazek.putpixel((x, y), filter_func(r, g, b))
 
-def inverse_filter():
-    for x in range(sirka):
-        for y in range(vyska):
-            r, g, b = obrazek.getpixel((x, y))
-            obrazek.putpixel((x, y), (255 - r, 255 - g, 255 - b))
+def green_filter(r, g, b):
+    return g, r, b
 
-def pink_filter():
-    for x in range(sirka):
-        for y in range(vyska):
-            r, g, b = obrazek.getpixel((x, y))
-            obrazek.putpixel((x, y), (int(r * 1.2), 0, int(b * 1.2)))    
+def inverse_filter(r, g, b):
+    return 255 - r, 255 - g, 255 - b
 
-def red_filter():
-    for x in range(sirka):
-        for y in range(vyska):
-            r, g, b = obrazek.getpixel((x, y))
-            obrazek.putpixel((x, y), (r, 0, 0))
+def pink_filter(r, g, b):
+    return int(r * 1.2), 0, int(b * 1.2)
 
-def blue_filter():
-    for x in range(sirka):
-        for y in range(vyska):
-            r, g, b = obrazek.getpixel((x, y))
-            obrazek.putpixel((x, y), (0 , 0, b))  
+def red_filter(r, g, b):
+    return r, 0, 0
 
-def cyberpunk_filter():
-    for x in range(sirka):
-        for y in range(vyska):
-            r, g, b = obrazek.getpixel((x, y))
-            obrazek.putpixel((x, y), (min(int(r * 1.5), 255), min(int(g * 0.5), 255), min(int(b * 2.0), 255)))                          
+def blue_filter(r, g, b):
+    return 0, 0, b
+
+def cyberpunk_filter(r, g, b):
+    return min(int(r * 1.5), 255), min(int(g * 0.5), 255), min(int(b * 2.0), 255)
 
 print('Hello!')
 print('Which filter do you want to use?')
 choice = input("1. Green\n2. Inverse\n3. Pink\n4. Red\n5. Blue\n6. Cyberpunk\nEnter your choice: ")
 
+filters = {
+    "1": green_filter,
+    "2": inverse_filter,
+    "3": pink_filter,
+    "4": red_filter,
+    "5": blue_filter,
+    "6": cyberpunk_filter
+}
+
 obrazek = Image.open("dog.jpg")
 sirka, vyska = obrazek.size
 
-if choice == "1":
-    green_filter()
-elif choice == "2":
-    inverse_filter()
-elif choice == "3":
-    pink_filter()
-elif choice == "4":
-    red_filter() 
-elif choice == "5":
-    blue_filter()
-elif choice == "6":
-    cyberpunk_filter()            
-
-                   
-
-
+if choice in filters:
+    apply_filter(filters[choice])
 
 obrazek.save("modified_dog.jpg")
 obrazek_modified = Image.open("modified_dog.jpg")
